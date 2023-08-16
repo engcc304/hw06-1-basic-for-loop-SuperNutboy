b799cd711e70436dda4227d5d770291f55181485
SuperNutboy <natthapong_ta66@live.rmutl.ac.th>
2023-08-17 01:28:28 +0700

    ๊ยหเหเำหเำเหเำหUPppppppp

--- Homework/HW06/hw06-1-basic-for-loop.cpp
+++ Homework/HW06/hw06-1-basic-for-loop.cpp
@@ -30,3 +30,21 @@
         [7] Hello World
 
 */
+#include <stdio.h>
+
+int main() {
+    int num; 
+
+    printf("Enter a number: ");
+    scanf("%d", &num); 
+
+    for (int i = 1; i <= num; i++) { 
+        if (i % 2 == 0) {
+            printf("(%d) Hello World\n", i); 
+        } else {
+            printf("[%d] Hello World\n", i); 
+        }
+    }
+
+    return 0;
+}
